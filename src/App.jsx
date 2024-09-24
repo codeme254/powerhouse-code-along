dc450d2009a5730263be3843759911d654fd0f1d
codeme254 <otwomadennis365@gmail.com>
2024-09-24 11:59:25 +0300

    build the header and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-
   return (
     <>
+      <Header />
       <h1>PowerHouse, Welcome</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
