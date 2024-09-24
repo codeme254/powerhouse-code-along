d2c32422d1ec798c82919d48455888916cb56b58
codeme254 <otwomadennis365@gmail.com>
2024-09-24 15:55:40 +0300

    build the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Catalogue from "./components/Catalogue/Catalogue";
 import Testimonies from "./components/Testimonies/Testimonies";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Catalogue />
       <Testimonies />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        &copy; 2024. All rights reserved. Proudly built by{" "}
+        <a href="https://github.com/codeme254">Dennis otwoma</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
