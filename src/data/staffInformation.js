106257a1f2a2924e7431268b4de9eea45e4e4f8b
kingjames500 <jimmymaina500@gmail.com>
2024-09-30 23:19:22 +0300

    creating staff data

---
+++ src/data/staffInformation.js
@@ -0,0 +1,37 @@
+import james from "../assets/staffImages/james.jpg";
+import benjamin from "../assets/staffImages/benjamin.jpg";
+import amelia from "../assets/staffImages/amelia.jpg";
+import charlotte from "../assets/staffImages/charlotte.jpg";
+
+const staffInformation = [
+  {
+    staffImage: james,
+    staffName: "James Maina",
+    staffTitle: "Chief Executive Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: benjamin,
+    staffName: "Benjamin Njuguna",
+    staffTitle: "Chief Technology Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: amelia,
+    staffName: "Amelia Njeri",
+    staffTitle: "Chief Marketing Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: charlotte,
+    staffName: "Charlotte Wanjiru",
+    staffTitle: "Chief Financial Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+];
+
+export default staffInformation;
