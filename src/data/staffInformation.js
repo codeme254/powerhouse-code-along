8041c3e7dabfe1f858876eb61652b97f647a6188
kingjames500 <jimmymaina500@gmail.com>
2024-10-03 11:59:32 +0300

    creating staff data

---
+++ src/data/staffInformation.js
@@ -0,0 +1,72 @@
+import james from "../assets/staffImages/james.jpg";
+import benjamin from "../assets/staffImages/benjamin.jpg";
+import amelia from "../assets/staffImages/amelia.jpg";
+import charlotte from "../assets/staffImages/charlotte.jpg";
+import grace from "../assets/staffImages/grace.jpg";
+import kenneth from "../assets/staffImages/kenneth.jpg";
+import mugure from "../assets/staffImages/mugure.jpg";
+import kiama from "../assets/staffImages/kiama.jpg";
+
+const staffInformation = [
+  {
+    staffImage: james,
+    staffName: "James Maina",
+    staffTitle: "Chief Executive Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: benjamin,
+    staffName: "Benjamin Njuguna",
+    staffTitle: "Chief Technology Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: amelia,
+    staffName: "Amelia Njeri",
+    staffTitle: "Chief Marketing Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: charlotte,
+    staffName: "Charlotte Wanjiru",
+    staffTitle: "Chief Financial Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+  {
+    staffImage: grace,
+    staffName: "Grace Wanjiru",
+    staffTitle: "Chief Operations Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+
+  {
+    staffImage: kenneth,
+    staffName: "Kenneth Karani",
+    staffTitle: "Chief Development Officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+
+  {
+    staffImage: mugure,
+    staffName: "miriam mugure",
+    staffTitle: "chairlady of the board",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+
+  {
+    staffImage: kiama,
+    staffName: "Kiama Wilfred",
+    staffTitle: "board of member",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, ducimus.",
+  },
+];
+
+export default staffInformation;
